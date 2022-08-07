5111fa51d2cec0706501e7014bdf1d1c8bb5d60f
MichaelHapgood <michael.hapgood@students.plymouth.ac.uk>
2022-08-07 14:51:05 +0100

    Environment Setup

---
+++ public class Main {.java
@@ -0,0 +1,8 @@
+public class Main {
+    for(int i = 0; i < 10; i++){
+        int g = 2;
+        int total = g + i
+    }
+    System.out.println(total)
+    
+}
---
+++ src/UnitTests/src/com/company/App.java
@@ -0,0 +1,16 @@
+package com.company;
+public class App{
+    
+private static int total;
+
+public static void main(String[] args) {
+    for(int i = 0; i < 10; i++){
+        int g = 2;
+        total = g * i;
+
+    }
+    System.out.println("total " + total);
+}
+
+
+}
